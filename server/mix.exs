5edb740170a6f00f458137b72212579024c83180
kriti-g <kriti7000@gmail.com>
2021-04-02 13:09:27 -0400

    comma and mix.lock

--- server/mix.exs
+++ server/mix.exs
@@ -44,7 +44,7 @@ defmodule RoommateApp.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
       {:cors_plug, "~> 2.0"},
       {:argon2_elixir, "~> 2.0"},
       {:password_validator, "~> 0.4"}
