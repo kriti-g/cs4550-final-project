adbd0f1e51831b6dda3c77467303b69d55748007
kriti-g <kriti7000@gmail.com>
2021-04-05 23:10:12 -0400

    group

--- web-ui/src/Group/Show.js
+++ web-ui/src/Group/Show.js
@@ -2,7 +2,7 @@ import { Col, Row } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { useRouteMatch } from 'react-router-dom';
 
-import { fetch_group } from '../api';
+import { fetch_group, fetch_user } from '../api';
 
 function ShowOneGroup({group, session}) {
     let member_list = group.users.map((usr) => {
@@ -40,19 +40,23 @@ function ShowOneGroup({group, session}) {
     );
 }
 
-function ShowGroup({group, session}) {
-    let match = useRouteMatch();
-    let id = match.params.id;
-    if(group && group.id === id && group.users.includes(session.user_id)) { // TODO
-        return (<ShowOneGroup group={group} session={session}/>);
-    } else if(group && group.id === id && session) {
-        return (<h6>You don't have access to this group</h6>);
-    } else if(session) {
-        fetch_group(id);
-        return (<h6>Loading group...</h6>);
+function ShowGroup({group, user, session}) {
+    if (session && user && group && group.id === user.group_id) {
+      return (<ShowOneGroup group={group} session={session}/>);
+    } else if (session && user && group) {
+      // somehow there is a cached group that the user can't access???
+      return (<h6>You don't have access to this group. Please try logging out and logging in.</h6>);
+    } else if (session && user && user.group_id && user.group_id > 0) {
+      fetch_group(user.group_id);
+      return (<h6>Loading group...</h6>);
+    } else if (session && user && (!user.group_id || user.group_id < 0)) {
+      return (<h6>Make or join a group to see it here!</h6>);
+    } else if (session) {
+      fetch_user(session.user_id);
+      return (<h6>Loading user info...</h6>);
     } else {
-        return (<h6>Sign up or login to see your group!</h6>);
+      return (<h6>Sign up or login to see your group!</h6>);
     }
 }
 
-export default connect(({group, session}) => ({group, session}))(ShowGroup);
+export default connect(({group, user, session}) => ({group, user, session}))(ShowGroup);
