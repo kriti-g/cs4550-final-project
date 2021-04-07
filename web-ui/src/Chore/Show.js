3c9e3874ed3039832b7daf89496fbba2e00f0344
daseyun <daseyun@gmail.com>
2021-04-07 13:49:15 -0400

    unique key

--- web-ui/src/Chore/Show.js
+++ web-ui/src/Chore/Show.js
@@ -1,60 +1,55 @@
-import { Col, Row } from 'react-bootstrap';
-import { connect } from 'react-redux';
-import { useRouteMatch } from 'react-router-dom';
+import { Col, Row } from "react-bootstrap";
+import { connect } from "react-redux";
+import { useRouteMatch } from "react-router-dom";
 
-import { fetch_chore } from '../api';
+import { fetch_chore } from "../api";
 
-function ShowOneChore({chore, session}) {
-    return (
-        <Row>
-            <Col>
-                <ul>
-                    <li>
-                        <strong>Name:</strong>
-                        {chore.name}
-                    </li>
-                    <li>
-                        <strong>Description:</strong>
-                        {chore.desc}
-                    </li>
-                    <li>
-                        <strong>Rotation:</strong>
-                        {chore.rotation}
-                    </li>
-                    <li>
-                        <strong>Frequency:</strong>
-                        {chore.frequency}
-                    </li>
-                    <li>
-                        <strong>Responsible:</strong>
-                        <ul>
-                            {chore.responsibilities.map((rsp) => {
-                                return (
-                                    <li>
-                                        {rsp.user.name}
-                                    </li>
-                                );
-                            })}
-                        </ul>
-                    </li>
-                </ul>
-            </Col>
-        </Row>
-    );
+function ShowOneChore({ chore, session }) {
+  return (
+    <Row>
+      <Col>
+        <ul>
+          <li>
+            <strong>Name:</strong>
+            {chore.name}
+          </li>
+          <li>
+            <strong>Description:</strong>
+            {chore.desc}
+          </li>
+          <li>
+            <strong>Rotation:</strong>
+            {chore.rotation}
+          </li>
+          <li>
+            <strong>Frequency:</strong>
+            {chore.frequency}
+          </li>
+          <li>
+            <strong>Responsible:</strong>
+            <ul>
+              {chore.responsibilities.map((rsp) => {
+                return <li key={rsp.id}>{rsp.user.name}</li>;
+              })}
+            </ul>
+          </li>
+        </ul>
+      </Col>
+    </Row>
+  );
 }
 
-function ShowChore({chore, session}){
-    let match = useRouteMatch();
-    let id = match.params.id;
-    if(chore && chore.id == id) {
-        return (<ShowOneChore chore={chore} session={session}/>);
-    } else if(session && chore === null) {
-        fetch_chore(id);
-        return (<h6>Loading Chore...</h6>);
-    } else {
-        return (<h6>You must be signed in to view this chore</h6>);
-    }
+function ShowChore({ chore, session }) {
+  let match = useRouteMatch();
+  let id = match.params.id;
+  if (chore && chore.id == id) {
+    return <ShowOneChore chore={chore} session={session} />;
+  } else if (session && chore === null) {
+    fetch_chore(id);
+    return <h6>Loading Chore...</h6>;
+  } else {
+    return <h6>You must be signed in to view this chore</h6>;
+  }
 }
 
-
-export default connect(({chore, session})=>({chore, session}))(ShowChore); // TODO
+export default connect(({ chore, session }) => ({ chore, session }))(ShowChore); // TODO
