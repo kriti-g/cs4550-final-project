075820bd1e2769b0d53418c093974f88d04f025c
Ben Wakefield <wenbakefield@gmail.com>
2021-04-09 12:06:31 -0400

    location front-end

--- web-ui/src/socket.js
+++ web-ui/src/socket.js
@@ -1,21 +1,38 @@
 import { Socket } from 'phoenix-socket';
-import { fetch_group } from './api'
+import { fetch_group, fetch_chore, fetch_user } from './api'
 
-let socket = new Socket("ws://localhost:4000/socket", {
-  // no authentication yet just ignore this
-  params: {
-    token: '123',
-  },
-});
+let socket = new Socket("ws://localhost:4000/socket", { params: {}});
 socket.connect();
 
 let channel = null;
+let user_id = null;
 let group_id = null;
+let chore_id = null;
 
+export function check_channel() {
+  return channel;
+}
+export function clear_channel() {
+  channel = null;
+}
 
+export function check_chore() {
+  return chore_id;
+}
+
+export function set_chore(cid) {
+  chore_id = cid
+}
 
 function state_update(resp) {
+  fetch_user(user_id);
   fetch_group(group_id);
+  if (resp.chore_id === chore_id) {
+    chore_id = "deleted"
+  }
+  if (chore_id && chore_id !== "deleted") {
+    fetch_chore(chore_id)
+  }
 }
 
 export function gen_socket(uid, token) {
@@ -28,18 +45,15 @@ export function gen_socket(uid, token) {
   socket.connect();
 }
 
-export function check_channel() {
-  return channel;
+export function listen_for_deletions(cb) {
+  channel.on("delete", cb);
 }
 
-export function clear_channel() {
-  channel = null;
-}
-
-// set the channel with gamename and join.
-export function join_group_channel(gid) {
+// set the channel with gid and join
+export function join_group_channel(uid, gid) {
+  user_id = uid;
   group_id = gid;
-  channel = socket.channel("live_group:" + group_id, {});
+  channel = socket.channel("live_group:" + group_id, {user: user_id});
   join_group();
 }
 
@@ -47,7 +61,18 @@ export function channel_signal() {
   channel.push("update", {})
 }
 
-// join game. called after gamename is set.
+export function reset_cb_bindings() {
+  channel.off("delete");
+  channel.off("update");
+  channel.on("delete", state_update);
+  channel.on("update", state_update);
+}
+
+export function channel_signal_deletion(cid) {
+  console.log("choreid", cid)
+  channel.push("delete", { chore_id: cid })
+}
+// join game. called after group id is set
 export function join_group() {
   channel
     .join()
@@ -59,6 +84,7 @@ export function join_group() {
     });
 
   // bind to listen to broadcasts.
+  channel.on("delete", state_update);
   channel.on("update", state_update);
 }
 
@@ -66,4 +92,10 @@ export function sendLoc(loc) {
     channel.push("location", loc)
         .receive("ok", resp => { console.log("Location successfully sent", resp) })
         .receive("error", resp => console.log("Error sending location", resp))
+    let messagesContainer = document.querySelector("#messages")
+    channel.on("nearby", payload => {
+        let messageItem = document.createElement("p")
+        messageItem.innerText = `${payload}`
+        messagesContainer.appendChild(messageItem)
+})
 }
