5d1535770d2166bf570131f1fb56e5e41b3ec97c
Ben Wakefield <wenbakefield@gmail.com>
2021-04-09 12:07:03 -0400

    location front-end

--- web-ui/src/Chore/Show.js
+++ web-ui/src/Chore/Show.js
@@ -2,18 +2,32 @@ import { Col, Row, Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { useRouteMatch } from 'react-router-dom';
 import store from "../store";
+import { delete_chore } from "../api"
+import { useHistory } from "react-router-dom";
+import {
+  join_group_channel,
+  channel_signal,
+  check_channel,
+  check_chore,
+  set_chore,
+  channel_signal_deletion,
+  listen_for_deletions,
+} from "../socket"
 import { sendLoc } from '../socket.js'
 
 import { fetch_chore, update_bulk_responsibilites } from '../api';
 
 function ShowOneChore({chore, session}) {
+  let history = useHistory();
   let rem_completions = completions_left();
   let next_deadline = deadline();
   function completions_left() {
-    if (chore.responsibilities.length === 0) {
-      return chore.rotation
+    if (chore.rotation === 0) {
+      return "Chore does not rotate."
+    } else if (chore.responsibilities.length === 0) {
+      return "Completions left before new rotation:" + chore.rotation
     } else {
-      return chore.rotation - chore.responsibilities[0].completions
+      return "Completions left before new rotation:" + (chore.rotation - chore.responsibilities[0].completions)
     }
   }
   function deadline() {
@@ -24,12 +38,29 @@ function ShowOneChore({chore, session}) {
     }
   }
 
+  function delete_cb(resp) {
+    if (resp.chore_id === chore.id) {
+      set_chore("deleted")
+      history.push("/group")
+      store.dispatch({ type: "chore/clear", data: {} });
+    }
+  }
+
+  if (!check_channel()) {
+    join_group_channel(session.user_id, chore.group_id)
+  }
+  if (!(check_chore() === chore.id)) {
+    set_chore(chore.id)
+  }
+  listen_for_deletions(delete_cb);
+
     return (
         <Row>
             <Col>
+                <div id="messages" role="log" aria-live="polite"></div>
                 <h4> {chore.name} </h4>
                 <p>Complete by {next_deadline}</p>
-                <p>Completions left before new rotation: {rem_completions}</p>
+                <p>{rem_completions}</p>
                 <p>Chore is repeated every {chore.frequency} hour(s).</p>
                 <p>{chore.desc}</p>
                 <h6>Members responsible:</h6>
@@ -53,12 +84,22 @@ function ShowOneChore({chore, session}) {
 function ChoreControls({chore, session}) {
   let is_responsible = chore.responsibilities.some((rsp) => {
       return rsp.user.id === session.user_id; });
-  function delete_chore() {
+
+  function on_delete() {
+    delete_chore(chore.id).then((rsp) => {
+      if (rsp.error) {
+        // if receiving an error, display it.
+        store.dispatch({ type: "error/set", data: rsp.error });
+      } else {
+        // update this chore
+        channel_signal_deletion(chore.id)
+      }
+    });
   }
 
   function format_loc(loc) {
     let location = {
-        timestamp: loc.timestamp,
+        chore: chore.id,
         latitude: loc.coords.latitude,
         longitude: loc.coords.longitude
     };
@@ -87,44 +128,41 @@ function ChoreControls({chore, session}) {
         store.dispatch({ type: "error/set", data: rsp.error });
       } else {
         // update this chore
-        fetch_chore(chore.id)
+        channel_signal()
       }
     });
   }
   let delete_button = (
     <Button
     variant="danger"
-    onClick={(ev) => delete_chore()}>
+    onClick={(_ev) => on_delete()}>
     Delete
     </Button>
   )
   let check_in_button = is_responsible ? (
     <Button
     variant="primary"
-    onClick={(ev) => check_in()}>
+    onClick={(_ev) => check_in()}>
     Check in
     </Button>
   ) : (<></>)
   let edit_button = is_responsible ? (
     <Button
     variant="success"
-    onClick={(ev) => mark_complete()}>
+    onClick={(_ev) => mark_complete()}>
     Mark complete
     </Button>
   ) : (<></>)
 
-  return (<div>
-          <>{edit_button}</>
-          <>{check_in_button}</>
-          </div>)
+  return (<>{delete_button}{edit_button}{check_in_button}</>)
 }
 
 function ShowChore({chore, session}){
     let match = useRouteMatch();
     let id = match.params.id;
-    if(chore && chore.id == id) {
+    if(chore && chore.id === parseInt(id) && check_chore() !== "deleted") {
         return (<ShowOneChore chore={chore} session={session}/>);
-    } else if(session && chore === null) {
+    } else if (session && (chore === null || chore.id !== parseInt(id))) {
         fetch_chore(id);
         return (<h6>Loading Chore...</h6>);
     } else {
